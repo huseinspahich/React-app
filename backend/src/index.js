f70a52b0fadf484820e60a95786ca2674c172de8
Harun Huseinspahic <harunhuseinspahic2001@gmail.com>
2024-11-13 20:12:20 +0100

    Added backend folder

---
+++ backend/src/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cors from "cors";
+import db from "./config/db.js";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/notes", async(req,res) => {
+    try {
+        const result = await db.query("SELECT * FROM biljeske");
+        res.json(result.rows);
+    }  catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.status(500).send('Greška pri učitavanju bilješki');
+   } 
+});
+
+app.post('/add-note', async (req, res) => {
+    const { naslov, sadrzaj } = req.body;
+    if (!naslov || !sadrzaj) {
+      return res.status(400).send('Naslov i sadržaj su obavezni');
+    }
+    try {
+      const result = await db.query('INSERT INTO biljeske (naslov, sadrzaj) VALUES ($1, $2)', [naslov, sadrzaj]);
+      res.status(201).send('Bilješka dodana');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Greška pri dodavanju bilješke');
+    }
+  });
+
+  app.delete('/delete-note/:id', async (req, res) => {
+    const { id } = req.params;  
+    try {
+      const result = await db.query('DELETE * FROM biljeske WHERE id = $1', [id]);
+      res.status(200).send('Bilješka obrisana');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Greška pri brisanju bilješke');
+    }
+  });
+
+app.listen(port, ()=> {
+    console.log(`Listenening at port ${port}`)
+});
