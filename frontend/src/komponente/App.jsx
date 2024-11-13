2d9860d2bb3545a0965f51c8b68086cc9e9f8d8d
Harun Huseinspahic <harunhuseinspahic2001@gmail.com>
2024-11-13 22:34:42 +0100

    Added connection between backend and frontend

--- frontend/src/komponente/App.jsx
+++ frontend/src/komponente/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "axios";
 import Header from "./Header";
 import Footer from "./Footer";
 import KreirajBiljeske from "./KreirajBiljeske";
@@ -8,31 +9,51 @@ import { ThemeProvider } from './ThemeContext';
 function App() {
   const [biljeske, postaviBiljeske] = useState([]);
 
-  function dodajBiljesku(biljeska) {
-    console.log("Dodavanje bilješke:", biljeska);
-    postaviBiljeske((prevValue) => {
-      return [...prevValue, biljeska];
-    });
-  }
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/notes");
+        postaviBiljeske(response.data); 
+      } catch (error) {
+        console.error("Greška pri preuzimanju bilješki:", error);
+      }
+    };
+    fetchNotes();
+  }, []);
 
-  function obrisiBiljesku(id) {
-    postaviBiljeske((prevValue) => {
-      return prevValue.filter((biljeska, index) => {
-        return index !== id;
+  const dodajBiljesku = async (biljeska) => {
+    try {
+      const response = await axios.post("http://localhost:3000/add-note", biljeska);
+      console.log(response.data); // Odgovor sa servera
+      postaviBiljeske((prevValue) => {
+        return [...prevValue, biljeska]; // Dodavanje nove bilješke
       });
-    });
-  }
+    } catch (error) {
+      console.error("Greška pri dodavanju bilješke:", error);
+    }
+  };
+
+  const obrisiBiljesku = async (id) => {
+    try {
+      const response = await axios.delete(`http://localhost:3000/delete-note/${id}`);
+      console.log(response.data); // Odgovor sa servera
+      postaviBiljeske((prevValue) => {
+        return prevValue.filter((biljeska) => biljeska.id !== id); // Brisanje bilješke sa odgovarajućim ID-jem
+      });
+    } catch (error) {
+      console.error("Greška pri brisanju bilješke:", error);
+    }
+  };
 
   return (
     <ThemeProvider>
       <Header />
       <KreirajBiljeske dodaj={dodajBiljesku} />
-      {biljeske.map((biljeska, index) => {
+      {biljeske.map((biljeska) => {
         return (
           <Biljeska
-            key={index}
-            index={index}
-            id={index}
+            key={biljeska.id}
+            id={biljeska.id}
             naslov={biljeska.naslov}
             sadrzaj={biljeska.sadrzaj}
             obrisi={obrisiBiljesku}
